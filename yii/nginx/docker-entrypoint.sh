5074533a6af5284b85d1495b8f56e90e66f76b89
Naba Das <nabad600@gmail.com>
2022-12-07 16:51:44 +0530

    advanced installation

--- yii/nginx/docker-entrypoint.sh
+++ yii/nginx/docker-entrypoint.sh
@@ -18,7 +18,7 @@ if [[ "$(ls -A "/var/www/")" ]] ;
         echo "If the Directory is not empty, please delete the hidden files and directory"
     else
         sudo composer create-project --prefer-dist yiisoft/yii2-app-advanced .
-        php init --env=Development --overwrite=All --delete=All
+        sudo php init --env=Development --overwrite=All --delete=All
         sudo cp /app/index.php /var/www/
         sudo cp /app/favicon.ico /var/www/
 fi
